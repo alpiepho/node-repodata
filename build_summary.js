295c8178053bb59acdce180c97af32093f12f786
al <alpiepho@yahoo.com>
2020-08-20 14:38:49 -0600

    changeed to generate details for new Github special README.md

--- build_summary.js
+++ build_summary.js
@@ -2,6 +2,8 @@
 // TODO: roll this to "better gh profile page???"
 
 var axios = require("axios")
+var fs = require('fs');
+
 
 var MAX_GHAPI_PAGEPER = 100
 var MAX_GHAPI_MAXPAGES = 2
@@ -39,7 +41,7 @@ function addExtra(array) {
   })
 }
 
-// github api isnt letting em get private repos, created private.json by hand
+// github api isnt letting me get private repos, created private.json by hand
 function addPrivate(array) {
   privateRepos.forEach((element) => {
     array.push(element)
@@ -66,7 +68,9 @@ function getCounts(array) {
   return [privateCount, publicCount, forkCount]
 }
 
-function finish(header, jsonData) {
+
+
+function finishMDX(header, jsonData) {
   addExtra(jsonData)
   addPrivate(jsonData)
   jsonData = sortByKey(jsonData, "name")
@@ -75,8 +79,10 @@ function finish(header, jsonData) {
   [privateCount, publicCount, forkCount] = getCounts(jsonData)
   publicCount -= forkCount
 
+  fileData = ""
+
   header +=
-    "\
+  "\
   \n\
   **NOTE:** This page is generated from a set of JSON data gathered from Github.\n\
   \n\
@@ -86,7 +92,7 @@ function finish(header, jsonData) {
   \n\
   \n"
 
-  console.log(header)
+  fileData += header + "\n"
 
   // DEBUG: dump names in order so they can be cut/paste
   // jsonData.forEach(element => {
@@ -111,31 +117,110 @@ function finish(header, jsonData) {
         '<span style="color:grey;font-weight:200">[' +
         element.language.toLowerCase() +
         "]</span> "
-    console.log(
-      "### " + (index + 1) + ") " + preString + element.name + languageStr
-    )
+    fileData += "### " + (index + 1) + ") " + preString + element.name + languageStr + "\n"
     if (element.png) {
-      console.log(
-        "[](../assets/github-repos__screenshot-" + element.id + ".png)"
-      )
+      fileData += "[](../assets/github-repos__screenshot-" + element.id + ".png)" + "\n"
     }
     if (element.hasREADMEmd) {
-      console.log(
-        "[" + element.name + "](" + element.html_url + "/blob/master/README.md)"
-      )  
+      fileData += "[" + element.name + "](" + element.html_url + "/blob/master/README.md)" + "\n"
     } else {
-      console.log(
-        "[" + element.name + "](" + element.html_url + ")"
-      )  
+      fileData += "[" + element.name + "](" + element.html_url + ")" + "\n"
     }
     description = "(see link)"
     if (element.description) description = element.description
-    console.log(": " + description)
-    if (element.notes) console.log("- " + element.notes)
-    if (element.ideas) console.log("- " + element.ideas)
-    console.log("")
+    fileData += ": " + description + "\n"
+    if (element.notes) fileData += "- " + element.notes + "\n"
+    if (element.ideas) fileData += "- " + element.ideas + "\n"
+    fileData += "" + "\n"
   })
+  fs.writeFileSync("github-summary.mdx", fileData)
 }
+
+
+function finishSpecialReadme(header, jsonData) {
+  // addExtra(jsonData)
+  // addPrivate(jsonData)
+  jsonData = sortByKey(jsonData, "name")
+  //console.log(JSON.stringify(jsonData, null, 2))
+
+  let privateCount, publicCount, forkCount
+  [privateCount, publicCount, forkCount] = getCounts(jsonData)
+  publicCount -= forkCount
+
+  fileData = ""
+
+  fileData += "\n### All Public Repositories Alphabetically\n\n<sup><sub>(using special repo to show all at once.  pinned and chart below)</sub></sup>\n\n(" + jsonData.length + " Total, " + publicCount + " Public, " + forkCount + " Forks)<br>\n"
+  today = new Date()
+  fileData += "<sup><sub>(updated " + today + ")</sub></sup>\n"
+  fileData += "\n"
+
+  jsonData.forEach((element, index) => {
+    subscriptStr = "(public)"
+    if (element.fork) subscriptStr = "(fork)"
+    if (element.hasREADMEmd) {
+      fileData += "[" + element.name + "](" + element.html_url + "/blob/master/README.md) <sup><i>" + subscriptStr + "</i></sup> <br>" + "\n"
+    } else {
+      fileData += "[" + element.name + "](" + element.html_url + ") <sup><i>" + subscriptStr + "</i></sup> <br>" + "\n"
+    }
+    description = "(see link)"
+    if (element.description) description = element.description
+    fileData += description + "<br>" + "\n"
+
+    switch(element.language) {
+      case "C":
+        fileData += "![##545454](https://placehold.it/15/545454/000000?text=+) Javascript<br>" + "\n"
+        break;
+      case "C++":
+        fileData += "![##F34B7C](https://placehold.it/15/f34b7/000000?text=+) Javascript<br>" + "\n"
+        break;
+      case "C#":
+        fileData += "![##178600](https://placehold.it/15/178600/000000?text=+) Javascript<br>" + "\n"
+        break;
+      case "CSS":
+        fileData += "![##58407E](https://placehold.it/15/58407e/000000?text=+) Javascript<br>" + "\n"
+        break;
+      case "Go":
+        fileData += "![##00ADD8](https://placehold.it/15/00add8/000000?text=+) Javascript<br>" + "\n"
+        break;
+      case "HTML":
+        fileData += "![##E34C27](https://placehold.it/15/e34c27/000000?text=+) Javascript<br>" + "\n"
+        break;
+      case "Java":
+        fileData += "![##B07218](https://placehold.it/15/b07218/000000?text=+) Javascript<br>" + "\n"
+        break;
+      case "JavaScript":
+        fileData += "![#F1E05A](https://placehold.it/15/f1e05a/000000?text=+) Javascript<br>" + "\n"
+        break;
+      case "PHP":
+        fileData += "![##4F5D94](https://placehold.it/15/4f5d94/000000?text=+) Javascript<br>" + "\n"
+        break;
+      case "Python":
+        fileData += "![##3571A5](https://placehold.it/15/3571a5/000000?text=+) Javascript<br>" + "\n"
+        break;
+      case "Shell":
+        fileData += "![##89E050](https://placehold.it/15/89e050/000000?text=+) Javascript<br>" + "\n"
+        break;
+      case "TypeScript":
+        fileData += "![##2B7489](https://placehold.it/15/2b7489/000000?text=+) Javascript<br>" + "\n"
+        break;
+      default:
+        fileData += "![#FFFFFF](https://placehold.it/15/ffffff/000000?text=+) unknown language<br>" + "\n"
+        break;
+    }
+    //TODO fix this
+    // localDate = Date(element.updated_at);
+    // console.log(element.updated_at)
+    // console.log(localDate.toString())
+    localDate = element.updated_at;
+    fileData += "<sup><sub>" + localDate + "</sub></sup><br>" + "\n"
+
+    // if (element.notes) fileData += "- " + element.notes + "\n"
+    // if (element.ideas) fileData += "- " + element.ideas + "\n"
+    fileData += "\n"
+  })
+  fs.writeFileSync("SpecialReadme.md", fileData)
+}
+
 
 let options = {}
 if (process.env.GH_API_TOKEN) {
@@ -160,7 +245,7 @@ for (i = 1; i <= MAX_GHAPI_MAXPAGES; i++) {
   )
 }
 
-//DEBUG: if you want to skip gathe of API above and the Promise.all below
+//DEBUG: if you want to skip gather of API above and the Promise.all below
 // this is cut/paste from running the following, manually in a browser
 // https://api.github.com/users/alpiepho/repos?per_page=100&page=1
 // https://api.github.com/users/alpiepho/repos?per_page=100&page=2
@@ -180,5 +265,8 @@ Promise.all(promises).then(() => {
       .catch(error => {})
     )
   })
-  Promise.all(promises).then(() => finish(header, jsonData))
+  Promise.all(promises).then(() => {
+    finishMDX(header, jsonData);
+    finishSpecialReadme(header, jsonData);
+  })
 })
